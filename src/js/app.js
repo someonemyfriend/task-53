961c624e5d23631448077460b897776d08d6ba42
Vasilia Staykova <someonemyfriend@gmail.com>
2023-01-15 14:16:39 +0200

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,15 @@
-import "../scss/app.scss";
+// import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const products = document.querySelector('.product');
+  products.forEach(product => {
+      const priceSpan = product.querySelector('.price');
+      const price = priceSpan.textContent;
+      product.setAttribute('data-price', price);
+  }) 
 
+  
 
 });
+
