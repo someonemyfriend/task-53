60b05b5d5adaf948f40d5b6935f392144a5bbf35
Vasilia Staykova <someonemyfriend@gmail.com>
2023-01-15 15:39:07 +0200

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,25 @@
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  const products = document.querySelector('.product');
-  products.forEach(product => {
-      const priceSpan = product.querySelector('.price');
-      const price = priceSpan.textContent;
-      product.setAttribute('data-price', price);
-  }) 
+
+
+  // const products = document.querySelector('.product');
+  // console.log(products);
+
+  // products.forEach(product => {
+  //     const priceSpan = product.querySelector('.price');
+  //     const price = priceSpan.textContent;
+  //     product.setAttribute('data-price', price);
+  // }) 
+
+  var productDiv = document.querySelector('.product');
+  var productPrice = document.querySelectorAll('p')[1].innerHTML;
+  var span = document.createElement('span');
+  span.class = "data-price";
+  span.className = "data-price";
+  span.innerText = productPrice;
+  productDiv.setAttribute('data-price', productPrice);
+  productDiv.appendChild(span); 
 
   
 
